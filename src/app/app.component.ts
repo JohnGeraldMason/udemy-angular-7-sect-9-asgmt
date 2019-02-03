70afd45d6782109426014a417e058a1c394087e9
JohnnyMatrixxx <JohnnyMatrixxx@users.noreply.github.com>
2019-02-03 16:28:08 -0400

    Added user-service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { UsersServiceService } from './users-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  activeUsers = ['Max', 'Anna'];
-  inactiveUsers = ['Chris', 'Manu'];
+export class AppComponent implements OnInit {
+  activeUsers = [];
+  inactiveUsers = [];
 
-  onSetToInactive(id: number) {
-    this.inactiveUsers.push(this.activeUsers[id]);
-    this.activeUsers.splice(id, 1);
-  }
-
-  onSetToActive(id: number) {
-    this.activeUsers.push(this.inactiveUsers[id]);
-    this.inactiveUsers.splice(id, 1);
+  constructor(private usersServiceService: UsersServiceService){}
+  
+  ngOnInit() {
+    this.activeUsers = this.usersServiceService.myActiveUsers;
+    this.inactiveUsers = this.usersServiceService.myInactiveUsers;
   }
 }
--- src/app/users-service.service.ts
+++ src/app/users-service.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class UsersServiceService {
 
-  constructor() { }
+  myActiveUsers = ['Max', 'Anna'];
+  myInactiveUsers = ['Chris', 'Manu'];
+
+  setToInactive(id: number) {
+    
+    this.myInactiveUsers.push(this.myActiveUsers[id]);
+    this.myActiveUsers.splice(id, 1);
+  }
+
+  setToActive(id: number) {
+    this.myActiveUsers.push(this.myInactiveUsers[id]);
+    this.myInactiveUsers.splice(id, 1);
+  }
+
 }
